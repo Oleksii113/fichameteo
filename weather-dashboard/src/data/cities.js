871217cd118c34e548dc28357348d68ba3fde2a2
Oleksii113 <olvelykyi23ig@epms.pt>
2026-05-06 09:38:12 +0000

    update

--- weather-dashboard/src/data/cities.js
+++ weather-dashboard/src/data/cities.js
@@ -1,8 +1,8 @@
 const cities = [
-    { id: "lisboa", name: "Lisboa", latitude: 38.7223, longtitude: -9.1393},
+    { id: "lisboa", name: "Lisboa", latitude: 38.7223, longitude: -9.1393 },
     { id: "porto", name: "Porto", latitude: 41.1579, longitude: -8.6291 },
-    { id: "coimbra", name: "Coimbra", latitude: 40.2033, longitude: -8.4103},
     { id: "faro", name: "Faro", latitude: 37.0194, longitude: -7.9304 },
+    { id: "coimbra", name: "Coimbra", latitude: 40.2033, longitude: -8.4103 },
     { id: "braga", name: "Braga", latitude: 41.5454, longitude: -8.4265 },
     { id: "evora", name: "Évora", latitude: 38.571, longitude: -7.9097 },
 ];
